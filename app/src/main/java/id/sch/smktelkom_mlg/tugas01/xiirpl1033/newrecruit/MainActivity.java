13f5663fb9f58f082f9c595b0300408ec5ba642b
reginaacintya <regina>
2016-09-09 11:55:03 +0700

    Menambahkan checkBox untuk type

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1033/newrecruit/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1033/newrecruit/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText etName;
     EditText etId;
     RadioButton rbV, rbM, rbI;
+    CheckBox cbWater, cbFire, cbGround, cbFly, cbGhost, cbDragon, cbGrass, cbBug, cbSteel;
     Button bOk;
     TextView tvResult;
 
@@ -26,7 +27,15 @@ public class MainActivity extends AppCompatActivity {
         rbV     =   (RadioButton) findViewById(R.id.radioButtonV);
         rbM     =   (RadioButton) findViewById(R.id.radioButtonM);
         rbI     =   (RadioButton) findViewById(R.id.radioButtonI);
-
+        cbBug   =   (CheckBox) findViewById(R.id.checkBoxBug);
+        cbDragon   =   (CheckBox) findViewById(R.id.checkBoxDragon);
+        cbFire   =   (CheckBox) findViewById(R.id.checkBoxFire);
+        cbFly    =   (CheckBox) findViewById(R.id.checkBoxFly);
+        cbGhost   =   (CheckBox) findViewById(R.id.checkBoxGhost);
+        cbGround   =   (CheckBox) findViewById(R.id.checkBoxGround);
+        cbGrass   =   (CheckBox) findViewById(R.id.checkBoxGrass);
+        cbSteel   =   (CheckBox) findViewById(R.id.checkBoxSteel);
+        cbWater   =   (CheckBox) findViewById(R.id.checkBoxWater);
         bOk =   (Button) findViewById(R.id.buttonOk);
         tvResult = (TextView) findViewById(R.id.textViewResult);
 
@@ -47,13 +56,23 @@ public class MainActivity extends AppCompatActivity {
                     gym = rbI.getText().toString();
                 }
 
+                String type = "Your Pokemon Type:\n";
+                int lentype = type.length();
+                if (cbBug.isChecked()) type+=cbBug.getText() + "\n";
+                if (cbDragon.isChecked()) type+=cbDragon.getText() + "\n";
+                if (cbFire.isChecked()) type+=cbFire.getText() + "\n";
+                if (cbFly.isChecked()) type+=cbFly.getText() + "\n";
+                if (cbGhost.isChecked()) type+=cbGhost.getText() + "\n";
+                if (cbGround.isChecked()) type+=cbGround.getText() + "\n";
+                if (cbGrass.isChecked()) type+=cbGrass.getText() + "\n";
+                if (cbSteel.isChecked()) type+=cbSteel.getText() + "\n";
+                if (cbWater.isChecked()) type+=cbWater.getText() + "\n";
 
-
-                if (name == null || id == null || gym == null) {
+                if (name == null || id == null || gym == null || type == null) {
                     tvResult.setText("You haven't fill the form completely");
                 }
                 else {
-                    tvResult.setText("You joined with us!, your info :" + "\nYour name is " + name + " with ID number " + id + "." + "\nYou are from team " + gym + " in ");
+                    tvResult.setText("You joined with us!, your info :" + "\nYour name is " + name + " with ID number " + id + "." + "\nYou are from team " + gym + " in " + "\n" + type);
                 }
             }
         });
