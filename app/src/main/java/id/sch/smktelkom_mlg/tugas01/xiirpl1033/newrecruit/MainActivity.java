b1b92370aff23723034511d7405443a7cd61848f
reginaacintya <regina>
2016-09-09 11:54:19 +0700

    Menambahkan radioButton untuk gym

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1033/newrecruit/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1033/newrecruit/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText etName;
     EditText etId;
+    RadioButton rbV, rbM, rbI;
     Button bOk;
     TextView tvResult;
 
@@ -22,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         etName  =   (EditText) findViewById(R.id.editName);
         etId    =   (EditText) findViewById(R.id.editId);
+        rbV     =   (RadioButton) findViewById(R.id.radioButtonV);
+        rbM     =   (RadioButton) findViewById(R.id.radioButtonM);
+        rbI     =   (RadioButton) findViewById(R.id.radioButtonI);
+
         bOk =   (Button) findViewById(R.id.buttonOk);
         tvResult = (TextView) findViewById(R.id.textViewResult);
 
@@ -31,9 +36,25 @@ public class MainActivity extends AppCompatActivity {
                 String name = etName.getText().toString();
                 String id = etId.getText().toString();
 
+                String gym = null;
+                if (rbV.isChecked()) {
+                    gym = rbV.getText().toString();
+                }
+                else if(rbM.isChecked()) {
+                    gym = rbM.getText().toString();
+                }
+                else if(rbI.isChecked()) {
+                    gym = rbI.getText().toString();
+                }
 
-                    tvResult.setText("You joined with us!, your info :" + "\nYour name is " + name + " with ID number " + id + ".");
 
+
+                if (name == null || id == null || gym == null) {
+                    tvResult.setText("You haven't fill the form completely");
+                }
+                else {
+                    tvResult.setText("You joined with us!, your info :" + "\nYour name is " + name + " with ID number " + id + "." + "\nYou are from team " + gym + " in ");
+                }
             }
         });
 
